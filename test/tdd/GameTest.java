b132f4bf5dbb9233fa4453f1abe02971dac4b707
Lulu <l.culenaire@gmail.com>
2026-04-16 18:46:13 +0200

    iter 1

---
+++ test/tdd/GameTest.java
@@ -0,0 +1,45 @@
+package tdd;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class GameTest {
+	Game game;
+	
+	@BeforeEach
+	void init() {
+		game=new Game();
+	}
+	
+	@Test
+	void test_entree_vide() {
+		assertEquals(0,game.score());
+	}
+	
+	@Test
+	void test_aucune_quille() {
+		for(int i=0;i<20;i++)
+			game.roll(0);
+		assertEquals(0,game.score());
+	}
+	
+	@Test
+	void test_une_quille() {
+		for(int i=0;i<20;i++)
+			game.roll(1);
+		assertEquals(20,game.score());
+	}
+	
+	@Test
+	void test_une_puis_deux_quille() {
+		for(int i=0;i<10;i++)
+			game.roll(1);
+		for(int i=0;i<10;i++)
+			game.roll(2);
+		assertEquals(30,game.score());
+	}
+	
+}
